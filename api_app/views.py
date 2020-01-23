dc8a184528f8751828bb2e3c4fe4f5c17b860793
Henri-Matias Tuomaala <henri-matias.tuomaala@aalto.fi>
2020-01-23 11:06:57 +0200

    static files should work now

--- api_app/views.py
+++ api_app/views.py
@@ -7,29 +7,31 @@ from rest_framework.response import Response
 def index(request):
     return render(request, 'index.html')
 
-@api_view(['POST'])
+@api_view(['POST','GET'])
 @csrf_exempt
 def api_req(request):
-    post_data = request.data["text"]
-    post_data_no_spaces = post_data.replace(" ","")
-    words = post_data.split(" ")
-    word_count = 0
-    for i in words:
-        if i:
-            word_count += 1
-    char_count = {}
-    for ch in sorted(list(post_data_no_spaces)):
-        if ch.isalpha():
-            try:
-                char_count[ch] += 1
-            except KeyError:
-                char_count[ch] = 1
-    char_count_list = []
-    for key in char_count:
-        char_count_list.append({key:char_count[key]})
     if request.method == "POST":
+        post_data = request.data["text"]
+        post_data_no_spaces = post_data.replace(" ","")
+        words = post_data.split(" ")
+        word_count = 0
+        for i in words:
+            if i:
+                word_count += 1
+        char_count = {}
+        for ch in sorted(list(post_data_no_spaces)):
+            if ch.isalpha():
+                try:
+                    char_count[ch] += 1
+                except KeyError:
+                    char_count[ch] = 1
+        char_count_list = []
+        for key in char_count:
+            char_count_list.append({key:char_count[key]})
         return Response({"textLength":
                             {"withSpaces": len(post_data),
                              "withoutSpaces": len(post_data_no_spaces)},
                          "wordCount": word_count,
-                          "characterCount": char_count_list})
+                         "characterCount": char_count_list})
+    else:
+        return Response()
--- dream_broker_app/urls.py
+++ dream_broker_app/urls.py
@@ -6,4 +6,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("api_app.urls")),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
